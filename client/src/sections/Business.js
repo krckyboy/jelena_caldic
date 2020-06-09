c790aa115866d783033679903d7ed867c2c0a928
Dušan Todorović <dusan.todorovic.2016@gmail.com>
2020-06-09 19:03:03 +0200

    #2.1

---
+++ client/src/sections/Business.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import styled from 'styled-components'
+import SubHeading from '../components/SubHeading'
+
+const Container = styled.div`
+	background-color: #99b3cb;
+
+	p {
+		font-size: 16px;
+		line-height: 26px;
+	}
+
+	p:not(:last-child) {
+		margin-bottom: 32px;
+	}
+`
+
+const VideoContainer = styled.div`
+	margin-bottom: 32px;
+	width: 100%;
+
+	img {
+		width: 100%;
+		max-width: 800px;
+	}
+`
+
+export default () => {
+	return (
+		<section className='topMargin'>
+			<Container className='topBottomPadding'>
+				<div className='sideMargin'>
+					<SubHeading style={{ backgroundColor: '#527799' }}>
+						Saradnja
+					</SubHeading>
+					<p>
+						Quas molestias excepturi sint occaecati cupiditate non provident,
+						similique sunt in culpa qui officia deserunt mollitia animi, id est
+						laborum et dolorum fuga. Et harum quidem rerum facilis est et
+						expedita distinctio. quas molestias excepturi sint occaecati
+						cupiditate non provident, similique sunt in culpa qui officia
+						deserunt mollitia animi, id est laborum et dolorum fuga. Et harum
+						quidem rerum facilis est et expedita distinctio.
+					</p>
+					<VideoContainer>
+						<img src='/images/jelena3.jpg' alt='Jelena Ćaldić' />
+					</VideoContainer>
+					<p>
+						At vero eos et accusamus et iusto odio dignissimos ducimus qui
+						blanditiis praesentium voluptatum deleniti atque corrupti quos
+						dolores et quas molestias excepturi sint occaecati cupiditate non
+						provident, similique sunt in culpa qui officia deserunt mollitia
+						animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis
+						est et expedita distinctio.
+					</p>
+				</div>
+			</Container>
+		</section>
+	)
+}
