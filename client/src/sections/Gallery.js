e1ff0b83885130bc80f4827d4737db43e26fa153
Dušan Todorović <dusan.todorovic.2016@gmail.com>
2020-07-14 21:23:47 +0200

    #14 Deleting react-grid-gallery; having a regular grid + carousel (slider) for mobile.

--- client/src/sections/Gallery.js
+++ client/src/sections/Gallery.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'
 import styled from 'styled-components'
 import SubHeading from '../components/SubHeading'
-import Gallery from 'react-grid-gallery'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
@@ -16,107 +17,91 @@ const Container = styled.div`
 	}
 `
 
-const IMAGES = [
+const images = [
 	{
-		src: '/images/gallery/2-min.webp',
-		thumbnail: '/images/gallery/thumbnails/2-min.jpg',
-		thumbnailWidth: 226,
-		thumbnailHeight: 320,
+		src: '/images/gallery/9-min.webp',
 	},
 	{
 		src: '/images/gallery/3-min.webp',
-		thumbnail: '/images/gallery/thumbnails/3-min.jpg',
-		thumbnailWidth: 320,
-		thumbnailHeight: 179,
 	},
 	{
 		src: '/images/gallery/4-min.webp',
-		thumbnail: '/images/gallery/thumbnails/4-min.jpg',
-		thumbnailWidth: 320,
-		thumbnailHeight: 213,
 	},
 	{
-		src: '/images/gallery/9-min.webp',
-		thumbnail: '/images/gallery/thumbnails/9-min.jpg',
-		thumbnailWidth: 320,
-		thumbnailHeight: 174,
+		src: '/images/gallery/2-min.webp',
 	},
-	// 2. row
 	{
 		src: '/images/gallery/8-min.webp',
-		thumbnail: '/images/gallery/thumbnails/8-min.jpg',
-		thumbnailWidth: 320,
-		thumbnailHeight: 213,
 	},
 	{
 		src: '/images/gallery/11-min.jpg',
-		thumbnail: '/images/gallery/thumbnails/11-min.jpg',
-		thumbnailWidth: 320,
-		thumbnailHeight: 183,
-	},
-	{
-		src: '/images/gallery/10-min.jpg',
-		thumbnail: '/images/gallery/thumbnails/10-min.jpg',
-		thumbnailWidth: 320,
-		thumbnailHeight: 212,
-	},
-	{
-		src: '/images/gallery/6-min.webp',
-		thumbnail: '/images/gallery/thumbnails/6-min.jpg',
-		thumbnailWidth: 240,
-		thumbnailHeight: 320,
-	},
-	// 3. row
-	{
-		src: '/images/gallery/16-min.jpg',
-		thumbnail: '/images/gallery/thumbnails/16-min.jpg',
-		thumbnailWidth: 257,
-		thumbnailHeight: 320,
-	},
-	{
-		src: '/images/gallery/5-min.jpg',
-		thumbnail: '/images/gallery/thumbnails/5-min.jpg',
-		thumbnailWidth: 320,
-		thumbnailHeight: 148,
-	},
-	{
-		src: '/images/gallery/13-min.jpg',
-		thumbnail: '/images/gallery/thumbnails/13-min.jpg',
-		thumbnailWidth: 320,
-		thumbnailHeight: 213,
-	},
-	{
-		src: '/images/gallery/17-min.jpg',
-		thumbnail: '/images/gallery/thumbnails/17-min.jpg',
-		thumbnailWidth: 320,
-		thumbnailHeight: 190,
-	},
-	// 4. row
-	{
-		src: '/images/gallery/12-min.jpg',
-		thumbnail: '/images/gallery/thumbnails/12-min.jpg',
-		thumbnailWidth: 200,
-		thumbnailHeight: 116,
-	},
-	{
-		src: '/images/gallery/1-min.jpg',
-		thumbnail: '/images/gallery/thumbnails/1-min.jpg',
-		thumbnailWidth: 200,
-		thumbnailHeight: 116,
-	},
-	{
-		src: '/images/gallery/14-min.jpg',
-		thumbnail: '/images/gallery/thumbnails/14-min.jpg',
-		thumbnailWidth: 200,
-		thumbnailHeight: 174,
-	},
-	{
-		src: '/images/gallery/7-min.webp',
-		thumbnail: '/images/gallery/thumbnails/7-min.jpg',
-		thumbnailWidth: 200,
-		thumbnailHeight: 116,
 	},
 ]
+
+// << Images phone
+const imagesMappedCarousel = images.map((i, index) => (
+	<img key={index * Math.random()} src={i.src} alt={i.alt} />
+))
+
+// Hide when certain screen width
+const CarouselStyled = styled(Carousel)`
+	.slider-wrapper {
+		max-height: 60vh;
+		min-height: 350px;
+	}
+
+	@media (min-width: 900px) {
+		display: none;
+	}
+`
+
+function RadoviCarousel() {
+	return (
+		<CarouselStyled
+			style={styles}
+			showThumbs={false}
+			showStatus={false}
+			swipeScrollTolerance={50}
+			showArrows={false}
+			autoPlay={true}
+		>
+			{imagesMappedCarousel.map((i, index) => (
+				<div key={index * Math.random()}>{i}</div>
+			))}
+		</CarouselStyled>
+	)
+}
+// Images phone >>
+
+// << Images desktop
+const GalleryDesktop = styled.div`
+	display: none;
+	grid-template-columns: repeat(8, 1fr);
+	grid-template-rows: repeat(5, 10vw);
+	grid-gap: 32px;
+	width: 100%;
+	max-width: initial;
+
+	@media (min-width: 900px) {
+		display: grid;
+	}
+`
+
+const ImgDesktop = styled.img`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+`
+
+const imagesMappedDesktop = images.map((i, index) => (
+	<figure
+		key={index * Math.random()}
+		className={`gallery__item gallery__item--${index + 1}`}
+	>
+		<ImgDesktop src={i.src} alt={i.alt} />
+	</figure>
+))
+// Images desktop >>
 
 const GalleryContainer = styled.div`
 	display: block;
@@ -187,11 +172,8 @@ export default () => {
 							alt='Spinner'
 							display={galleryOpacity === 0 ? 'block' : 'none'}
 						/>
-						<Gallery
-							images={IMAGES}
-							rowHeight={264}
-							style={{ opacity: galleryOpacity }}
-						/>
+						<RadoviCarousel autoPlay={true} />
+						<GalleryDesktop>{imagesMappedDesktop}</GalleryDesktop>
 					</GalleryContainer>
 				</div>
 			</Container>
